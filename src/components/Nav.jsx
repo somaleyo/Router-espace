6936284268659bc21e5c51ac257834be27daddf7
Emil <somaleyoo123@gmail.com>
2025-05-27 10:57:37 +0200

    navbar css and text

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react'
+import './nav.css'
+
 export default function Nav(props) {
     
+    const[ishome,setIshome]=useState(true)
+    const[isdest,setIsdest]=useState(false)
+    const[iscrew,setIscrew]=useState(false)
+    const[istech,setIstech]=useState(false)
+
 
 
     return(
         <>
-            
+            <nav>
+                <img className='logo' src="/src/assets/images/assets/shared/logo.svg" alt="" />
+                <div className="tiret"></div>
+                <div className="ancres">
+                    <p className={ishome? "active":""}><span>00</span> HOME</p>
+                    <p className={isdest? "active2":""}><span>01</span> DESTINATION</p>
+                    <p className={iscrew? "active":""}><span>02</span> CREW</p>
+                    <p className={istech? "active2":""}><span>03</span> TECHNOLOGY</p>
+                </div>
+            </nav>
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from 'react'
 import './home.css'
 import { Link } from 'react-router-dom'
+import Nav from '../components/Nav'
 
 export default function Home(props) {
     
@@ -11,7 +12,7 @@ export default function Home(props) {
 return(
     <>
         <div className="home-container">
-            
+            <Nav/>
         </div>
     </>
     )
