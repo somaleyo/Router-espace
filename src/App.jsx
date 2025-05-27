39f3c7695a5cf225c74a785d215bb8baa3367ef6
Emil <somaleyoo123@gmail.com>
2025-05-27 09:25:46 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState,useState } from 'react'
+
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+
+function App() {
+  
+
+  return (
+    <>
+   
+    </>
+  )
+}
+
+export default App
