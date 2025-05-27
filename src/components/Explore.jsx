98ad4383bb1a54c48178589588a997ff2d693709
Emil <somaleyoo123@gmail.com>
2025-05-27 11:13:59 +0200

    explore

---
+++ src/components/Explore.jsx
@@ -0,0 +1,20 @@
+import './explore.css'
+export default function Explore(props) {
+    
+
+
+    return(
+        <>
+            <div className="explore">
+                <div className="explore-left">
+                    <h3>SO, YOU WANT TO TRAVEL TO</h3>
+                    <h1>SPACE</h1>
+                    <p>Let's face it: if you want to goto space , you might as well genuinely go to outer space and not hover kind of on the edge of it . Well sit back and relax beceause we'll give you a truly out of this world experience!</p>
+                </div>
+                <div className="explore-right">
+                    <button className='btn btn-explore'>EXPLORE</button>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import './home.css'
 import { Link } from 'react-router-dom'
 import Nav from '../components/Nav'
+import Explore from '../components/Explore'
 
 export default function Home(props) {
     
@@ -13,6 +14,7 @@ return(
     <>
         <div className="home-container">
             <Nav/>
+            <Explore/>
         </div>
     </>
     )
