7d8df009c9e958181a29f18d134cb4c7170d4e5b
Emil <somaleyoo123@gmail.com>
2025-05-28 09:51:57 +0200

    ansou

--- src/pages/Crew/Ansari.jsx
+++ src/pages/Crew/Ansari.jsx
@@ -6,12 +6,15 @@ export function Ansari(params) {
               <div className="crew">
              <div className="crew-container">
                     <div className="crew-left">
-                        <h4>COMMANDER</h4>
-                        <h3>DOUGLAS HURLEY</h3>
-                        <p className='text'>Douglas Gerald Hurly is and American engineer, former marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2</p>
+                        <h4>FLIGHT INEER</h4>
+                        <h3>ANOUSHEH ANSARI</h3>
+                        <p className='text'>
+  Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. 
+  Ansari was the fourth self-funded space tourist, the first self-funded woman to 
+  fly to the ISS, and the first Iranian in space. </p>
                         </div>
                         <div className="crew-right">
-                        <img className='foto' src="/src/assets/images/assets/crew/image-douglas-hurley.png" alt="" />
+                        <img className='foto' src="/src/assets/images/assets/crew/image-anousheh-ansari.png" alt="" />
                         
                         </div>
                     </div>
