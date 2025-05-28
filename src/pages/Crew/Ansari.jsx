89b88ce12c4ed5b77d370884ba5e21278bbc5d3f
Emil <somaleyoo123@gmail.com>
2025-05-28 09:50:16 +0200

    mark

--- src/pages/Crew/Ansari.jsx
+++ src/pages/Crew/Ansari.jsx
@@ -3,7 +3,19 @@ export function Ansari(params) {
 
     return(
         <>
-            
+              <div className="crew">
+             <div className="crew-container">
+                    <div className="crew-left">
+                        <h4>COMMANDER</h4>
+                        <h3>DOUGLAS HURLEY</h3>
+                        <p className='text'>Douglas Gerald Hurly is and American engineer, former marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2</p>
+                        </div>
+                        <div className="crew-right">
+                        <img className='foto' src="/src/assets/images/assets/crew/image-douglas-hurley.png" alt="" />
+                        
+                        </div>
+                    </div>
+                   </div>
         </>
     )
 }
--- src/pages/Crew/Mark.jsx
+++ src/pages/Crew/Mark.jsx
@@ -3,7 +3,21 @@ export function Mark(params) {
 
     return(
         <>
-            
+              <div className="crew">
+             <div className="crew-container">
+                    <div className="crew-left">
+                        <h4>MISSION SPECIALIST</h4>
+                        <h3>MARK SHUTTLEWORTH</h3>
+                        <p className='text'>  Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind 
+  the Linux-based Ubuntu operating system. Shuttleworth became the first South 
+  African to travel to space as a space tourist.</p>
+                        </div>
+                        <div className="crew-right">
+                        <img className='foto' src="/src/assets/images/assets/crew/image-mark-shuttleworth.png" alt="" />
+                        
+                        </div>
+                    </div>
+                   </div>
         </>
     )
 }
