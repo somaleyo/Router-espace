4b00307de365f62f7073a0f68170f3f9417dd3d3
Emil <somaleyoo123@gmail.com>
2025-05-28 09:22:17 +0200

    europa

--- src/pages/Destination/Europa.jsx
+++ src/pages/Destination/Europa.jsx
@@ -3,7 +3,26 @@ export default function Europa() {
 
     return(
         <>
-            
+            <div className="destination-content">
+         <div className="destination-gauche">
+                            <img className='planete' src="/src/assets/images/destination/europa.png" alt="" />
+
+                        </div>
+            <div className="destination-droite">
+                <h1>EUROPA</h1>
+                            <p className='text'>The smallest of the four Galilean moons orbiting Jupiter, Europa is a 
+  winter lover’s dream. With an icy surface, it’s perfect for a bit of 
+  ice skating, curling, hockey, or simple relaxation in your snug 
+  wintery cabin.
+                            </p>
+                                <hr  className='dest'/>
+                            <div className="bottom-dest">
+                            
+                            <p><span className="traveltime">EST.TRAVEL TIME</span></p>
+                            <h5>3 DAYS</h5>
+                            </div>
+            </div>
+            </div>
         </>
     )
 }
--- src/pages/Destination/Titan.jsx
+++ src/pages/Destination/Titan.jsx
@@ -3,7 +3,25 @@ export default function Titan() {
 
     return(
         <>
-            
+            <div className="destination-content">
+         <div className="destination-gauche">
+                            <img className='planete' src="/src/assets/images/destination/Titan.png" alt="" />
+
+                        </div>
+            <div className="destination-droite">
+                <h1>TITAN</h1>
+                            <p className='text'> The only moon known to have a dense atmosphere other than Earth, Titan 
+  is a home away from home (just a few hundred degrees colder!). As a 
+  bonus, you get striking views of the Rings of Saturn.
+                            </p>
+                                <hr  className='dest'/>
+                            <div className="bottom-dest">
+                            
+                            <p><span className="traveltime">EST.TRAVEL TIME</span></p>
+                            <h5>7 YEARS</h5>
+                            </div>
+            </div>
+            </div>
         </>
     )
 }
